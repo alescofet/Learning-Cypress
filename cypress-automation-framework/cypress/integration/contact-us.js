513c077e7be8dd36465fb92376668231db8fcc26
alescofet <alex.escofet96@gmail.com>
2021-06-01 14:15:56 +0200

    First test done

---
+++ cypress-automation-framework/cypress/integration/contact-us.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress" />
+
+describe("Test contact-us form via WebDriverUni",() =>{
+    it("Should be able to submit a successful submission via contact-us form", ()=>{
+        cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        //cy.get('#contact-us').click({force:true})
+        cy.get('[name="first_name"]').type("Juan")
+        cy.get('[name="last_name"]').type("Martinez")
+        cy.get('[name="email"]').type("juan.martinez@juan.com")
+        cy.get('textarea.feedback-input').type("This is a test done with cypress")
+        cy.get('[type="submit"]').click()
+
+    });
+
+    it("Should not be able to submit a successful submission via contact-us form as all fields must be fullfilled", ()=>{
+        /* cypress code here */
+    });
+})
