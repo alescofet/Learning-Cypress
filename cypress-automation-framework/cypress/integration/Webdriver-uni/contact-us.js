c4ac09b5b2da6f1205d43fd1c20b951194636074
alescofet <alex.escofet96@gmail.com>
2021-06-07 15:58:12 +0200

    Chai assertions referencing windows exercise done

--- cypress-automation-framework/cypress/integration/Webdriver-uni/contact-us.js
+++ cypress-automation-framework/cypress/integration/Webdriver-uni/contact-us.js
@@ -3,6 +3,9 @@
 describe("Test contact-us form via WebDriverUni",() =>{
     it("Should be able to submit a successful submission via contact-us form", ()=>{
         cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+        cy.title().should('include', 'Contact Us')
+        cy.url().should('include', 'contactus')
         //cy.get('#contact-us').click({force:true})
         cy.get('[name="first_name"]').should('to.be.empty')
         cy.get('[name="first_name"]').type("Juan")
