caf2b722acaf342ef3b534df91c82d4fea3f7333
alescofet <alex.escofet96@gmail.com>
2021-06-11 13:18:18 +0200

    Concatenation of cypress actions

---
+++ cypress-automation-framework/cypress/integration/Automation-test-store/inspect-item.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+
+describe("Inspect automation test store items using chaining",() =>{
+    it("Should click on the first item using item header", ()=>{
+        cy.visit("https://automationteststore.com")
+        cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .fixed_wrapper > .fixed > .prdocutname').click()
+
+
+    });
+    it("Should click on the first item using item text", ()=>{
+        cy.visit("https://automationteststore.com")
+        cy.get('.prdocutname').contains('Skinsheen').click()
+
+
+    });
+    it("Should click on the first item using item index", ()=>{
+        cy.visit("https://automationteststore.com")
+        cy.get('.fixed_wrapper').find('.prdocutname').eq(0).click()
+        
+
+
+    });
+
+
+})
